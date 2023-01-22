f99e8343677397f97bff67b0900bd2b322c46c39
Yfons <yfons123@gmail.com>
2023-01-22 03:10:47 +0100

    Projektdateien hinzufügen.

---
+++ BfnXmlReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+using System.Xml;
+
+namespace BfnXmlReaderLibrary;
+public static class BfnXmlReader
+{
+    public static T ReadXml<T>(string xmlFile, XmlReaderSettings settings, XmlSerializer serializer)
+        where T : new()
+    {
+        try
+        {
+            using var fileStream = new FileStream(xmlFile, FileMode.Open);
+            using var reader = XmlReader.Create(fileStream, settings);
+            return (T)serializer.Deserialize(reader)!;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("MOD EXCEPTION: XML was not loaded\n" + ex.ToString());
+            return new T();
+        }
+    }
+}
